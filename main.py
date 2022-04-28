d93d5b9f1c0730bbf1bb91cf9097781c6cc925a9
mrserjo <mrserjo@mail.ru>
2022-04-28 18:19:33 +0300

    hw

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ import sqlite3
 
 conn = sqlite3.connect('FNS-MSP-2019-sg2.sqlite3', check_same_thread=False)
 
+cursor = conn.cursor()
+cursor.execute("""
+    SELECT * FROM dohrash
+    ORDER BY СумДоход
+    LIMIT 25;
+""")
+records = cursor.fetchall()
+for row in records:
+    print('INN:', row[1])
+print(records)
 
-res = requests.get('FNS-MSP-2019-sg2.sqlite3')
-soup = BeautifulSoup()
+cursor.close()
+
+
+# res = requests.get('FNS-MSP-2019-sg2.sqlite3')
+# soup = BeautifulSoup()
 
